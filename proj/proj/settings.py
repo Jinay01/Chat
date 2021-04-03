bae45dad9fe4320dc7fc836106e1e01fb67a7fc2
Jinay01 <jinayparekh2001@gmail.com>
2021-04-03 11:48:05 +0530

    basic setup done asgi.py is left

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'proj.wsgi.application'
-
+ASGI_APPLICATION = "asgi.asgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
